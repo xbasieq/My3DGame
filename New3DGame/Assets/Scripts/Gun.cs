1f69ceeca663a2a246ea9a036fe842b3979b0e12
xbasieq <78031735+xbasieq@users.noreply.github.com>
2021-05-25 17:27:08 +0200

    Step Three

--- New3DGame/Assets/Scripts/Gun.cs
+++ New3DGame/Assets/Scripts/Gun.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
+
 {
+    public Rigidbody Bullet;
+    public Transform firePlace;
+    public float power;
+
+    private Rigidbody projectile;
+    private bool fire;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,20 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            projectile = Instantiate(Bullet, firePlace.position, transform.rotation);
+            fire = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (fire)
+        {
+            projectile.velocity = transform.TransformDirection(Vector3.forward * power);
+            fire = false;
+            Destroy(projectile.gameObject, 5);
+        }
     }
 }
